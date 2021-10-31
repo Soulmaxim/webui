1e4bec894d5d5ea21e7d12572cd567656aa0413b
BorisovMaxim <soulmaxim@gmail.com>
2021-10-31 04:34:18 +0400

    Initial commit

---
+++ src/test/java/com/geekbrains/webui/AppTest.java
@@ -0,0 +1,14 @@
+package com.geekbrains.webui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void testForTest() {
+        System.out.println("Something test words");
+        assertTrue(true);
+    }
+}
